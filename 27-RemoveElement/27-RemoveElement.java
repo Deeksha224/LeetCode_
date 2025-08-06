cb8422a2f802a770797c5666fe6b6a6b1656a2a2
Deeksha Agarwal <agarwaldeeksha224@gmail.com>
2025-08-06 11:12:37 +0530

    Create 27-RemoveElement

---
+++ 27-RemoveElement/27-RemoveElement.java
@@ -0,0 +1,14 @@
+// Last updated: 8/6/2025, 11:12:37 AM
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        for(int i = 0; i < nums.length; i++){
+            if (nums[i] == val){
+                count++;
+                nums[i] = Integer.MAX_VALUE;
+            }
+        }
+        Arrays.sort(nums);
+        return (nums.length - count);
+    }
+}
