3777225d8f8d386a216d9fd0974fda39e740ae13
Deeksha Agarwal <agarwaldeeksha224@gmail.com>
2025-08-06 11:07:32 +0530

    Create 1426-FindNUniqueIntegersSumUpToZero

---
+++ 1426-FindNUniqueIntegersSumUpToZero/1426-FindNUniqueIntegersSumUpToZero.java
@@ -0,0 +1,14 @@
+// Last updated: 8/6/2025, 11:07:33 AM
+class Solution {
+    public int[] sumZero(int n) {
+        int[] arr = new int[n];
+        int sum = 0;
+        for(int i = 0; i < n - 1; i++){
+            arr[i] = i + 1;
+            sum = sum + i + 1;
+        }
+        arr[n - 1] = (sum * -1);
+        return arr;
+    }
+    
+}
