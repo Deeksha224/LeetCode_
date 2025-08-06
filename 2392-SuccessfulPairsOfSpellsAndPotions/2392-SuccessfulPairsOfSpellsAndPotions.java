0086ff02982096870073aa1f8c6dabf28ccc0e30
Deeksha Agarwal <agarwaldeeksha224@gmail.com>
2025-08-06 11:05:35 +0530

    Create 2392-SuccessfulPairsOfSpellsAndPotions

---
+++ 2392-SuccessfulPairsOfSpellsAndPotions/2392-SuccessfulPairsOfSpellsAndPotions.java
@@ -0,0 +1,26 @@
+// Last updated: 8/6/2025, 11:05:36 AM
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int[] result = new int[spells.length];
+        Arrays.sort(potions);
+        
+        for (int i = 0; i < spells.length; i++) {
+            int left = 0;
+            int right = potions.length - 1;
+            int j = potions.length;
+
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if ((long) spells[i] * potions[mid] >= success) {
+                    j = mid;
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            result[i] = potions.length - j;
+        }
+
+        return result;
+    }
+}
