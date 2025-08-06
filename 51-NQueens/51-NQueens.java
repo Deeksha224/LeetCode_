0ac08f691926fffda6c38d248a113a75cf2ad4d9
Deeksha Agarwal <agarwaldeeksha224@gmail.com>
2025-08-06 11:11:54 +0530

    Create 51-NQueens

---
+++ 51-NQueens/51-NQueens.java
@@ -0,0 +1,68 @@
+// Last updated: 8/6/2025, 11:11:55 AM
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+      List<List<String>> li = new ArrayList<>();
+      boolean[][] board = new boolean[n][n];
+      Print(board, n,0,li);
+      return li;
+	}
+    public static void Print(boolean[][] board, int tq, int row , List<List<String>> li) {
+		if (tq == 0) {
+            List<String> l = new ArrayList<>();
+			for (int i = 0; i < row; i++) {
+                StringBuilder sb = new StringBuilder();
+                for (int j = 0; j < row; j++) {
+                    if (board[i][j]) {
+                        sb.append("Q");
+                    } else {
+                        sb.append(".");
+                    }
+                }
+                l.add(sb.toString());
+            }
+            li.add(l);
+            return;
+        }
+		
+		for (int col = 0; col < board[0].length; col++) {
+			if (issafe(board, row, col)) {
+				board[row][col] = true;
+				Print(board, tq - 1, row + 1, li);
+				board[row][col] = false;
+			}
+
+		}
+	}
+
+	public static boolean issafe(boolean[][] board, int row, int col) {
+		// up
+		int r = row;
+		while (r >= 0) {
+			if (board[r][col] == true) {
+				return false;
+			}
+			r--;
+		}
+		r = row;
+		int c = col;
+		// left daigonal
+		while (r >= 0 && c >= 0) {
+			if (board[r][c] == true) {
+				return false;
+			}
+			r--;
+			c--;
+		}
+		// right daigonal
+		r = row;
+		c = col;
+		while (r >= 0 && c < board[0].length) {
+			if (board[r][c] == true) {
+				return false;
+			}
+			r--;
+			c++;
+		}
+		return true;
+    }
+}
