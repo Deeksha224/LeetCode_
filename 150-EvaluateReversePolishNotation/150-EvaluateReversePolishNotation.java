34a55329a292f67b5320f6982367d73a34432929
Deeksha Agarwal <agarwaldeeksha224@gmail.com>
2025-08-06 11:11:02 +0530

    Create 150-EvaluateReversePolishNotation

---
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.java
@@ -0,0 +1,27 @@
+// Last updated: 8/6/2025, 11:11:03 AM
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack <Integer> st = new Stack<>();
+        for (String str : tokens){
+            if (str.equals("+")){
+                st.push(st.pop() + st.pop());
+            }
+            else if (str.equals("-")){
+                int temp = st.pop();
+                st.push(st.pop() - temp);
+            }
+            else if (str.equals("*")){
+                st.push(st.pop() * st.pop());
+            }
+            else if (str.equals("/")){
+                int temp = st.pop();
+                st.push(st.pop() / temp);
+            }
+            else{
+                st.push(Integer.parseInt(str));
+            }
+
+        }
+        return st.pop();
+    }
+}
