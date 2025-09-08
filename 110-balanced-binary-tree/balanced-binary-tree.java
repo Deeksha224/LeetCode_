6ee96a6b0f9ec2524ae2f1d1ce6c8a05b0c36588
Deeksha Agarwal <agarwaldeeksha224@gmail.com>
2025-09-08 19:28:33 +0530

    Time: 1 ms (23.54%) | Memory: 44.4 MB (57.43%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -13,25 +13,47 @@
  *     }
  * }
  */
+// class Solution {
+//     public boolean isBalanced(TreeNode root) {
+//         if (root == null){
+//             return true;
+//         }
+//         int left_h = height(root.left);
+//         int right_h = height(root.right);
+//         if (Math.abs(left_h - right_h) > 1){
+//             return false;
+//         }
+//         return isBalanced(root.left) && isBalanced(root.right);
+
+//     }
+//     public int height (TreeNode root){
+//         if (root == null){
+//             return -1;
+//         }
+//         int left = height(root.left);
+//         int right = height(root.right);
+//         return Math.max(left,right)+1;
+//     }
+// }
+
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if (root == null){
-            return true;
-        }
-        int left_height = helper(root.left);
-        int right_height = helper(root.right);
-        if (Math.abs(left_height - right_height) > 1){
-            return false;
-        }
-        return isBalanced(root.left) && isBalanced(root.right);
-
+        return balance(root).bal;
     }
-    public int helper (TreeNode root){
+    public Pair balance(TreeNode root){
         if (root == null){
-            return 0;
+            return new Pair();
         }
-        int left = helper(root.left);
-        int right = helper(root.right);
-        return Math.max(left,right) + 1;
+        Pair lb = balance(root.left);
+        Pair rb = balance(root.right);
+        Pair sp = new Pair();
+        sp.ht = Math.max(lb.ht, rb.ht)+1;
+        boolean sb = Math.abs(lb.ht-rb.ht)<=1;
+        sp.bal = lb.bal && rb.bal && sb;
+        return sp;
+    }
+    class Pair{
+        boolean bal = true;
+        int ht = -1;
     }
 }
