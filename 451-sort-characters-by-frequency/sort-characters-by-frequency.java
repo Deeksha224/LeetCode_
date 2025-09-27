9f1b0c5d9620a5915fb572748308fb835ded2f80
Deeksha Agarwal <agarwaldeeksha224@gmail.com>
2025-09-27 14:23:31 +0530

    Time: 14 ms (54.89%) | Memory: 45.9 MB (32.27%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,18 +1,49 @@
+// class Solution {
+//     public String frequencySort(String s) {
+//         int n=s.length();
+//         Map<Character, Integer> mp=new HashMap<>();
+//         for(int i=0; i<n; i++){
+//             mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);
+//         }
+//         List<Map.Entry<Character, Integer>> ll=new ArrayList<>(mp.entrySet());
+
+//         Collections.sort(ll,(a,b)->{
+//             return b.getValue()-a.getValue();
+//         });
+
+//         StringBuilder sb=new StringBuilder();
+//         for(Map.Entry<Character, Integer> i:ll){
+//             char ch=i.getKey();
+//             int t=i.getValue();
+//             for(int j=0; j<t; j++){
+//                 sb.append(ch);
+//             }
+//         }
+//         return sb.toString();
+
+//     }
+// }
+
+
+
+
+
 class Solution {
     public String frequencySort(String s) {
+
         int n=s.length();
         Map<Character, Integer> mp=new HashMap<>();
         for(int i=0; i<n; i++){
             mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0)+1);
         }
-        List<Map.Entry<Character, Integer>> ll=new ArrayList<>(mp.entrySet());
 
-        Collections.sort(ll,(a,b)->{
-            return b.getValue()-a.getValue();
-        });
+        PriorityQueue<Map.Entry<Character, Integer>> pq=new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+
+        pq.addAll(mp.entrySet());
 
         StringBuilder sb=new StringBuilder();
-        for(Map.Entry<Character, Integer> i:ll){
+        while(!pq.isEmpty()){
+            Map.Entry<Character, Integer> i=pq.poll();
             char ch=i.getKey();
             int t=i.getValue();
             for(int j=0; j<t; j++){
