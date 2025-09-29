d5bb04366d793d73bc8c62f37e97506389e7af62
Deeksha Agarwal <agarwaldeeksha224@gmail.com>
2025-09-29 13:43:22 +0530

    Time: 0 ms (100.00%) | Memory: 40.3 MB (84.14%) - LeetSync

--- 367-valid-perfect-square/valid-perfect-square.java
+++ 367-valid-perfect-square/valid-perfect-square.java
@@ -1,7 +1,20 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
-        double x = Math.sqrt(num);
-        int y = (int)Math.round(x);
-        return x == y;
+        long min = 0;
+        long hi = num;
+        while (min <= hi){
+            long mid = min +(hi-min)/2;
+
+            if (mid*mid == num){
+                return true;
+            }
+            else if ((mid*mid) > num){
+                hi = mid - 1;
+            }
+            else{
+                min = mid+1;
+            }
+        }
+        return false;
     }
 }
