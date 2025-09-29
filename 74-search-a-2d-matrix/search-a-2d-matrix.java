290d6d078b9131dc65482bed2d5f31258524bb0e
Deeksha Agarwal <agarwaldeeksha224@gmail.com>
2025-09-30 04:44:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (66.83%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,17 +1,46 @@
-class Solution {
-    public boolean searchMatrix(int[][] matrix, int target) {
-        int i = 0;
-        int j = matrix[0].length -1;
-        while (i < matrix.length && j >= 0){
+// complexity O(log(m+n))
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         int i = 0;
+//         int j = matrix[0].length -1;
+//         while (i < matrix.length && j >= 0){
+//             if (matrix[i][j] == target){
+//                 return true;
+//             }
+//             else if (matrix[i][j] > target){
+//                 j--;
+//             }
+//             else{
+//                 i++;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+// complexity O(log(n*m))
+class Solution{
+    public boolean searchMatrix(int[][] matrix, int target){
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int start = 0;
+        int end = m*n-1;
+        while (start <= end){
+            int mid = (start+end)/2;
+
+            int i = mid / n;
+            int j = mid % n;
+
             if (matrix[i][j] == target){
                 return true;
             }
             else if (matrix[i][j] > target){
-                j--;
+                end = mid - 1;
             }
             else{
-                i++;
+                start = mid +1;
             }
+            
         }
         return false;
     }
