85cb85fdc0872b0eb5a30508ab4549824ee8b225
Deeksha Agarwal <agarwaldeeksha224@gmail.com>
2025-09-30 11:18:42 +0530

    Time: 49 ms (50.72%) | Memory: 47.9 MB (34.53%) - LeetSync

---
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -0,0 +1,49 @@
+//Brute force (tle - tc= O(n^2))
+// class Solution {
+//     public int subarraysWithKDistinct(int[] nums, int k) {
+//         int n = nums.length;
+//         int count = 0;
+//         for (int i = 0; i < n; i++) {
+//             Map<Integer, Integer> map = new HashMap<>();
+//             for (int j = i; j < n; j++) {
+//                 map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+
+//                 if (map.size() == k) {
+//                     count++;
+//                 } else if (map.size() > k) {
+//                     break; // stop expanding
+//                 }
+//             }
+//         }
+
+//         return count;
+
+//     }
+// }
+
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return cntk(nums,k) - cntk(nums,k-1);
+    }
+
+    public int cntk(int[] nums, int k){
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int i = 0;
+        int j = 0;
+        int cnt = 0;
+        while (j < nums.length){
+            map.put(nums[j], map.getOrDefault(nums[j], 0) + 1);
+
+            while (map.size() > k){
+               map.put(nums[i], map.get(nums[i]) - 1);
+                if (map.get(nums[i]) == 0){
+                    map.remove(nums[i]);
+                }
+                i++;
+            }
+            cnt+=(j-i+1);
+            j++;
+        }
+        return cnt;
+    }
+}
