1ae440f9b884646834e18b062aa8e60b14fc3e90
Deeksha Agarwal <agarwaldeeksha224@gmail.com>
2025-10-06 21:21:58 +0530

    Time: 2 ms (100.00%) | Memory: 44.9 MB (88.19%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,10 +1,9 @@
 class Solution {
     public int minDistance(String word1, String word2) {
         int[][] dp = new int[word1.length()][word2.length()];
-        return Min_Ops(word1, word2,0,0,dp);
-
+        return Min_op(word1,word2,0,0,dp);
     }
-    public static int Min_Ops(String s, String t, int i, int j, int[][] dp){
+    public int Min_op (String s, String t, int i, int j, int[][] dp){
         if (i == s.length()){
             return t.length() - j;
         }
@@ -14,16 +13,15 @@ class Solution {
         if (dp[i][j] != 0){
             return dp[i][j];
         }
-
-        int ans = 0;
+       int ans = 0;
         if (s.charAt(i) == t.charAt(j)){
-           ans = Min_Ops(s,t,i+1,j+1,dp);
+            ans = Min_op(s,t,i+1,j+1,dp);
         }
         else{
-            int I =  Min_Ops(s,t,i,j+1,dp);
-            int D =  Min_Ops(s,t,i+1,j,dp);
-            int R =  Min_Ops(s,t,i+1,j+1,dp);
-            ans = Math.min(I,Math.min(D,R)) + 1;
+            int I = Min_op(s,t,i,j+1,dp);
+            int D = Min_op(s,t,i+1,j,dp);
+            int R = Min_op(s,t,i+1,j+1,dp);
+            ans = Math.min(I,Math.min(D,R)) +1;
         }
         return dp[i][j] = ans;
     }
