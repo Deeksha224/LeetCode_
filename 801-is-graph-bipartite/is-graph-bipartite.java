23c751d45b040a94cf8ae473d0da6faf04e30929
Deeksha Agarwal <agarwaldeeksha224@gmail.com>
2025-10-16 11:45:52 +0530

    Time: 8 ms (6.36%) | Memory: 45.4 MB (30.26%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean isBipartite(int[][] graph) {
+        // therefore question reduce to find the cycle is even or odd, if any cycle of odd length is found then not a bipartite else a bipartite graph.
+        Queue<BipartitePair> q = new LinkedList<>();
+        HashMap<Integer,Integer> visited = new HashMap<>();
+        for (int i = 0; i < graph.length; i++){
+            if(visited.containsKey(i)){
+                continue;
+            }
+            q.add(new BipartitePair(i,0));
+            while (!q.isEmpty()){
+                //1. remove
+                BipartitePair rp = q.poll();
+                //2. Ignore already visited
+                if (visited.containsKey(rp.vtx)){
+                    if (visited.get(rp.vtx) != rp.dis){
+                        return false;
+                    }
+                    continue;
+
+                }
+                //3.makred visited
+                visited.put(rp.vtx,rp.dis);
+                //4. self work
+                //5. Add unvisited nbrs
+                for (int nbrs:graph[rp.vtx]){
+                    if (!visited.containsKey(nbrs)){
+                        q.add(new BipartitePair(nbrs,rp.dis+1));
+                    }
+                }
+            }
+        }
+        return true; 
+    }
+}
+class BipartitePair{
+    int vtx;
+    int dis;
+    public BipartitePair(int vtx,int dis){
+        this.vtx = vtx;
+        this.dis=dis;
+    }
+}
