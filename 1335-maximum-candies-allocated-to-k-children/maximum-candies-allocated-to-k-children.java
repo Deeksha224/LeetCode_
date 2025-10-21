e7977999dd5c3656308a3147a00eeb0b8a45c8fa
Deeksha Agarwal <agarwaldeeksha224@gmail.com>
2025-10-21 16:41:09 +0530

    Time: 47 ms (7.19%) | Memory: 63.3 MB (35.15%) - LeetSync

--- 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
+++ 1335-maximum-candies-allocated-to-k-children/maximum-candies-allocated-to-k-children.java
@@ -1,15 +1,15 @@
 class Solution {
     public int maximumCandies(int[] nums, long k) {
         Arrays.sort(nums);
-        int start = 0;
+        int start = 1;
         int end = nums[nums.length-1];
         int ans = 0;
         
-        while (start < end){
-            int mid = (start+end+1)/2;
+        while (start <= end){
+            int mid = (start+end)/2;
             if (isPossible (nums,k,mid)){
                 ans = mid;
-                start = mid;
+                start = mid+1;
             }
             else{
                 end = mid-1;
