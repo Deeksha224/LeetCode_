1bb04bdd003ae3c8d1e1e6ae6ab0bcfcb5af9d2f
Deeksha Agarwal <agarwaldeeksha224@gmail.com>
2025-10-26 06:27:59 +0530

    Time: 3 ms (50.42%) | Memory: 42.4 MB (50.42%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String makeGood(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()){
+            if (!st.isEmpty() && Math.abs(c - st.peek()) == 32){
+                st.pop();
+            }
+            else{
+                st.push(c);
+            }
+        }
+        StringBuilder ans = new StringBuilder();
+        while (!st.isEmpty()){
+            ans.append(st.pop());
+        }
+        return ans.reverse().toString();
+    }
+}
