cbf5c8ba5b5f3c13b0cb3e1143498afdd44ddac7
Deeksha Agarwal <agarwaldeeksha224@gmail.com>
2025-11-06 12:00:09 +0530

    Time: 65 ms (43.77%) | Memory: 110.5 MB (6.38%) - LeetSync

--- 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.java
+++ 1792-find-the-most-competitive-subsequence/find-the-most-competitive-subsequence.java
@@ -2,9 +2,12 @@ class Solution {
     public int[] mostCompetitive(int[] nums, int k) {
         Stack<Integer> st = new Stack<>();
         for (int i = 0; i < nums.length; i++){
-            while (!st.isEmpty() && st.peek() > nums[i] && (st.size() + (nums.length - i)) > k){
+            while (!st.isEmpty() && st.peek() > nums[i] && (st.size() + (nums.length - i)) > k){ 
                 st.pop();
             }
+            // st.size() == size of current stack.
+            // nums.length - i == element remain in the array which can be put in stack
+            // its sum must be greater then sum only then we can pop a elememt.
             
             if (st.size() < k){
                 st.push(nums[i]);
