48b8c52312c03e6283c98b798b1db3e9e61b7d7b
Deeksha Agarwal <agarwaldeeksha224@gmail.com>
2026-01-19 16:18:46 +0530

    Time: 3 ms (74.83%) | Memory: 43.8 MB (20.11%) - LeetSync

---
+++ 682-baseball-game/baseball-game.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> st = new Stack<>();
+
+        for (String s : operations) {
+            if (s.equals("C")) {
+                st.pop();
+            } 
+            else if (s.equals("D")) {
+                st.push(st.peek() * 2);
+            } 
+            else if (s.equals("+")) {
+                int top = st.pop();
+                int newTop = top + st.peek();
+                st.push(top);
+                st.push(newTop);
+            } 
+            else {
+                st.push(Integer.parseInt(s));
+            }
+        }
+
+        int ans = 0;
+        while (!st.isEmpty()) {
+            ans += st.pop();
+        }
+        return ans;
+    }
+}
