cfd79dbe0378014ebe6abdd38a0b2cdc23535182
Deeksha Agarwal <deeksha.agarwal_cs23@gla.ac.in>
2026-02-28 15:25:53 +0530

    Time: 161 ms (36.05%) | Memory: 46.3 MB (23.26%) - LeetSync

--- 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
@@ -4,23 +4,10 @@ class Solution {
         long ans = 0;
         int bits = 0;
         for(int i = 1; i<=n; i++){
-            if((i & (i-1)) == 0) bits++;
+           // if((i & (i-1)) == 0) bits++;
+            bits = Integer.toBinaryString(i).length();
             ans = ((ans<<bits)+i)%mod;
         }
         return (int)ans;
     }
 }
-
-
-// class Solution {
-//     long mod = 1000000007L;
-//     public int concatenatedBinary(int n) {
-//         String ans = "";
-//         for(int i = 1; i <= n; i++){
-//             String temp = Integer.toBinaryString(i);
-//             ans += temp;
-//         }
-//         long a = Long.parseLong(ans)%mod;
-//         return (int)a;
-//     }
-// }
