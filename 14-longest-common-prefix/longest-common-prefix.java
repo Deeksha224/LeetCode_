9629eefde805dd9d521ea55dfd146a2465e56cb5
Deeksha Agarwal <deeksha.agarwal_cs23@gla.ac.in>
2026-03-13 16:28:17 +0530

    Time: 1 ms (64.93%) | Memory: 43.1 MB (91.03%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder s = new StringBuilder();
+        Arrays.sort(strs);
+        String a = strs[0];
+        String b = strs[strs.length-1];
+        for(int i = 0; i < a.length(); i++){
+            if(a.charAt(i)==b.charAt(i)){
+                s.append(a.charAt(i));
+            }
+            else{
+                break;
+            }
+        }
+        return s.toString();
+    }
+}
