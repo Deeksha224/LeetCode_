56d5d495547d66066597e18395e278b5ef06f3e1
Deeksha Agarwal <deeksha.agarwal_cs23@gla.ac.in>
2026-03-24 22:23:34 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (97.38%) - LeetSync

--- 112-path-sum/path-sum.java
+++ 112-path-sum/path-sum.java
@@ -16,11 +16,11 @@
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
         if(root == null) return false;
-        if(root.left == null && root.right == null){
-            return targetSum-root.val==0;
+        if(root.left==null && root.right==null){
+            if(targetSum-root.val==0) return true;
         }
-        boolean left = hasPathSum(root.left, targetSum-root.val);
-        boolean right = hasPathSum(root.right, targetSum-root.val);
+        boolean left = hasPathSum(root.left,targetSum-root.val);
+        boolean right = hasPathSum(root.right,targetSum-root.val);
         return left||right;
     }
 }
