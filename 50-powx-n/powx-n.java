d8a6b86ccfd02fe4da3af7eaf3a27b23eaeb49b6
Deeksha Agarwal <deeksha.agarwal_cs23@gla.ac.in>
2026-07-31 16:44:12 +0530

    Time: 0 ms (100.00%) | Memory: 48.2 MB (22.49%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int N) {
+        long n = N;
+        if(n < 0){
+            x = 1/x;
+            n = -n;
+        }
+        double ans = 1;
+        while(n > 0){
+            if(n%2 != 0){
+                ans = ans*x;
+            }
+            x*=x;
+            n/=2;
+        }
+        return ans;
+        
+    }
+}
