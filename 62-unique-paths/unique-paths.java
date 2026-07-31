9dcd1076a09607688677a31260754b050b305219
Deeksha Agarwal <deeksha.agarwal_cs23@gla.ac.in>
2026-07-31 21:47:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (13.18%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,35 +1,15 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        int board[][] = new int[m][n];
-        // 0 means not visited
-        // 1 means visitedboard
-
-        int dp[][] = new int [m][n];
-        for(int arr[] :dp){
-            Arrays.fill(arr,-1);
-        }
-        return helper(0,0,board,dp);
-        
+        int[][] dp = new int[m][n];
+        return count(m-1,n-1, dp);
     }
+    int count (int row, int col, int[][] dp){
+        if(row == 0|| col == 0) return 1;
 
-    public int helper(int i, int j, int board[][], int dp[][]){
-        if(i>=board.length || j>=board[0].length || board[i][j]==1){
-            return 0;
+        if(dp[row][col] != 0){
+            return dp[row][col];
         }
-        if(i==board.length-1 && j==board[0].length-1){
-            return 1;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-
-        board[i][j]=1;
-        int ans =0;
-        ans+=helper(i,j+1,board,dp);
-        ans+=helper(i+1,j,board,dp);
-        board[i][j]=0;
-
-        return dp[i][j]=ans;
-
+        dp[row][col] = count(row-1,col,dp)+ count(row,col-1,dp);
+        return dp[row][col];
     }
 }
