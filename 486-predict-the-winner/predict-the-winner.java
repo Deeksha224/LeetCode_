48392daaa2dfc720485207d806710f9035d33e5e
Deeksha Agarwal <deeksha.agarwal_cs23@gla.ac.in>
2026-08-03 11:32:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (65.92%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,6 +1,10 @@
 class Solution {
     public boolean predictTheWinner(int[] nums) {
-        int ans1 = solve(0,nums.length-1, nums);
+        int[][] dp = new int[nums.length][nums.length];
+        for(int[]d:dp){
+            Arrays.fill(d,-1);
+        }
+        int ans1 = solve(0,nums.length-1, nums,dp);
         int sum = 0;
         for(int i = 0; i < nums.length; i++){
             sum+=nums[i];
@@ -10,12 +14,17 @@ class Solution {
         return false; 
         
     }
-    public int solve(int i, int j, int[] nums){
+    public int solve(int i, int j, int[] nums,int[][]dp){
         if(i>j) return 0;
-        int takei = nums[i] + Math.min(solve(i+2,j,nums) , solve(i+1,j-1,nums));
-        int takej = nums[j]+ Math.min(solve(i+1,j-1,nums) , solve(i,j-2,nums));
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        }
+        int takei = nums[i] + Math.min(solve(i+2,j,nums,dp) , solve(i+1,j-1,nums,dp));
+        int takej = nums[j]+ Math.min(solve(i+1,j-1,nums,dp) , solve(i,j-2,nums,dp));
 
-        return Math.max(takei,takej);
+        dp[i][j] = Math.max(takei,takej);
+        return dp[i][j];
+
 
     }
 }
