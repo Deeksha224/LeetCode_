e19203284a7587192b6298ffa9b91123eff747f5
Deeksha Agarwal <deeksha.agarwal_cs23@gla.ac.in>
2026-08-05 15:00:43 +0530

    Time: 39 ms (38.54%) | Memory: 52.3 MB (20.31%) - LeetSync

---
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean winnerSquareGame(int n) {
+        Boolean[] dp = new Boolean[n+1];
+        return fun(n,dp);
+    }
+    public boolean fun (int n, Boolean[] dp){
+        if(n==0) return false;
+        if(dp[n] != null) return dp[n];
+        for (int i = 1; i*i <=n; i++){
+            if(!fun(n - i*i,dp)) {
+                return dp[n]=true;
+            }
+        }
+        return dp[n]=false;
+    }
+}
