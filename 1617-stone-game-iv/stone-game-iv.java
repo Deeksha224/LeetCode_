dd0b1160d123adf81fe2d4dfc2e879067a457504
Deeksha Agarwal <deeksha.agarwal_cs23@gla.ac.in>
2026-08-06 23:57:04 +0530

    Time: 40 ms (37.16%) | Memory: 52.7 MB (16.55%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.java
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -3,12 +3,12 @@ class Solution {
         Boolean[] dp = new Boolean[n+1];
         return fun(n,dp);
     }
-    public boolean fun (int n, Boolean[] dp){
+    public boolean fun(int n, Boolean[] dp){
         if(n==0) return false;
         if(dp[n] != null) return dp[n];
-        for (int i = 1; i*i <=n; i++){
-            if(!fun(n - i*i,dp)) {
-                return dp[n]=true;
+        for (int i = 1; i*i <= n; i++){
+            if(fun(n-i*i,dp) == false){
+                return dp[n] = true;
             }
         }
         return dp[n]=false;
