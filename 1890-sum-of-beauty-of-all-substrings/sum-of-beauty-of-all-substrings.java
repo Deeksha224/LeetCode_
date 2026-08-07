e1f893378e3bc89164747c5960adc90a7317a8f7
Deeksha Agarwal <deeksha.agarwal_cs23@gla.ac.in>
2026-08-07 20:11:03 +0530

    Time: 60 ms (78.27%) | Memory: 44.3 MB (36.58%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -1,42 +1,27 @@
 class Solution {
     public int beautySum(String s) {
-        return count(fun(s));
+        return count(s);
     }
-
-    public int count(ArrayList<String> list){
+    public int count(String s){
         int ans=0;
-        for(String str : list){
-            int min = Integer.MAX_VALUE;
-            int max = Integer.MIN_VALUE;
+        for(int i = 0; i < s.length(); i++){
             int[] freq = new int[26];
-            for(int i = 0; i < str.length(); i++){
-                freq[str.charAt(i)-'a']++;
-            }
-            for(int f : freq){
-                if(f > 0){
-                    min = Math.min(min, f);
-                    max = Math.max(max, f);
+            for(int j = i; j < s.length(); j++){
+                freq[s.charAt(j)-'a']++;
+                int min = Integer.MAX_VALUE;
+                int max = Integer.MIN_VALUE;
+            
+                for(int f : freq){
+                    if(f > 0){
+                        min = Math.min(min, f);
+                        max = Math.max(max, f);
+                    }
                 }
-             }
-            //System.out.println(max+" min: "+ min);
-            ans += (max-min);
+                //System.out.println(max+" min: "+ min);
+                ans += (max-min);
+            }
 
         }
         return ans;
-    }
-    public ArrayList<String> fun (String s){
-        ArrayList<String> list = new ArrayList<>();
-        for(int i = 0; i < s.length(); i++){
-            for(int j = i+1; j<=s.length(); j++){
-                if(j-i >= 3)
-                    list.add(s.substring(i,j));
-            }
-        }
-
-        //ArrayList<String> list = new ArrayList<>(set);
-        // for(int i = 0; i < set.size(); i++){
-        //     System.out.println(list.get(i));
-        // }
-        return list;
     }
 }
