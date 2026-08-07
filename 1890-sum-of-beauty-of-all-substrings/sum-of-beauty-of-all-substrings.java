52df81f398463740e108a0300f7e09324352f62d
Deeksha Agarwal <deeksha.agarwal_cs23@gla.ac.in>
2026-08-07 20:24:04 +0530

    Time: 61 ms (63.14%) | Memory: 44.2 MB (46.89%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -1,26 +1,21 @@
 class Solution {
     public int beautySum(String s) {
-        return count(s);
-    }
-    public int count(String s){
-        int ans=0;
+        int ans = 0;
         for(int i = 0; i < s.length(); i++){
             int[] freq = new int[26];
             for(int j = i; j < s.length(); j++){
-                freq[s.charAt(j)-'a']++;
                 int min = Integer.MAX_VALUE;
                 int max = Integer.MIN_VALUE;
-            
-                for(int f : freq){
-                    if(f > 0){
-                        min = Math.min(min, f);
-                        max = Math.max(max, f);
+                freq[s.charAt(j)-'a']++;
+
+                for(int f:freq){
+                    if(f>0){
+                        min = Math.min(min,f);
+                        max = Math.max(max,f);
                     }
                 }
-                //System.out.println(max+" min: "+ min);
                 ans += (max-min);
             }
-
         }
         return ans;
     }
