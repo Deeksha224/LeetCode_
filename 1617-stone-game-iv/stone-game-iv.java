d9f5da7a2ec6ad66bf2b2cd07953853b3e3c65da
Deeksha Agarwal <deeksha.agarwal_cs23@gla.ac.in>
2026-08-10 15:37:03 +0530

    Time: 40 ms (35.25%) | Memory: 52.6 MB (15.14%) - LeetSync

--- 1617-stone-game-iv/stone-game-iv.java
+++ 1617-stone-game-iv/stone-game-iv.java
@@ -3,12 +3,15 @@ class Solution {
         Boolean[] dp = new Boolean[n+1];
         return fun(n,dp);
     }
-    public boolean fun(int n, Boolean[] dp){
+    public boolean fun (int n, Boolean[] dp){
         if(n==0) return false;
         if(dp[n] != null) return dp[n];
-        for (int i = 1; i*i <= n; i++){
-            if(fun(n-i*i,dp) == false){
-                return dp[n] = true;
+        for (int i = 1; i*i <=n; i++){
+
+            // i remove i^2 , and opponent recive (n-i^2), then i again call the function if the result give me true then ik its a bad move and i have to avoid it, same if i get a false ik its a good move for me making opponent to loose the game hence no need to check further so just can return the answer.
+
+            if(fun(n - i*i,dp)==false) { 
+                return dp[n]=true;
             }
         }
         return dp[n]=false;
