852be9b050a3406daf8e83d6af03c7582c9185ee
Deeksha Agarwal <deeksha.agarwal_cs23@gla.ac.in>
2026-08-27 00:25:08 +0530

    Time: 2 ms (40.94%) | Memory: 44 MB (58.39%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.java
@@ -1,40 +1,68 @@
+// class Solution {
+//     public String shortestBeautifulSubstring(String s, int k) {
+//         List<String> list = new ArrayList<>();
+//         int i = 0;
+//         while(i < s.length()){
+//             int j = i;
+//             int count = 0;
+//             while(j < s.length()){
+//                 if(s.charAt(j) == '1'){
+//                     count++;
+//                     if(count == k) {
+//                         String temp = s.substring(i,j+1);
+//                         if(!list.contains(temp))
+//                             list.add(temp);
+//                         count = 0;
+//                         break;
+//                     }
+//                 }
+//                 j++;
+//             }
+//             i++;
+//         }
+//         // First: shortest length
+//         // Second: lexicographically smallest
+//         list.sort((a, b) -> {
+
+//             if(a.length() != b.length())
+//                 return Integer.compare(a.length(), b.length());
+
+//             return a.compareTo(b);
+//         });
+
+//         if(list.size() == 0)
+//             return "";
+
+//         return list.get(0);
+       
+//     }
+
+// }
+
+//Keeping the best and discarting the rest
 class Solution {
     public String shortestBeautifulSubstring(String s, int k) {
-        List<String> list = new ArrayList<>();
-        int i = 0;
-        while(i < s.length()){
-            int j = i;
+        String ans = "";
+        for(int i = 0; i < s.length(); i++) {
             int count = 0;
-            while(j < s.length()){
-                if(s.charAt(j) == '1'){
+
+            for(int j = i; j < s.length(); j++) {
+                if(s.charAt(j) == '1') {
                     count++;
-                    if(count == k) {
-                        String temp = s.substring(i,j+1);
-                        if(!list.contains(temp))
-                            list.add(temp);
-                        count = 0;
-                        break;
-                    }
                 }
-                j++;
+                if(count == k) {
+                    String temp = s.substring(i, j + 1);
+                    if(ans.equals("") ||
+                       temp.length() < ans.length() ||
+                       (temp.length() == ans.length() &&
+                        temp.compareTo(ans) < 0)) {
+
+                        ans = temp;
+                    }
+                    break;
+                }
             }
-            i++;
         }
-        // First: shortest length
-        // Second: lexicographically smallest
-        list.sort((a, b) -> {
-
-            if(a.length() != b.length())
-                return Integer.compare(a.length(), b.length());
-
-            return a.compareTo(b);
-        });
-
-        if(list.size() == 0)
-            return "";
-
-        return list.get(0);
-       
+        return ans;
     }
-
 }
