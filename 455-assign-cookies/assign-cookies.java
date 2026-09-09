eed4602f3f344364f2ed6b9466b3507ed399fd17
Deeksha Agarwal <deeksha.agarwal_cs23@gla.ac.in>
2026-09-09 12:51:20 +0530

    Time: 13 ms (99.27%) | Memory: 51.4 MB (74.28%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -1,12 +1,12 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        int count = 0;
         Arrays.sort(g);
         Arrays.sort(s);
-        int i = g.length-1;
-        int j = s.length-1;
-        while (i >=0 && j >=0){
-            if (g[i] <= s[j]){
+        int count = 0;
+        int i = g.length -1;
+        int j = s.length - 1;
+        while(i >= 0 && j >= 0){
+            if(s[j] >= g[i]){
                 count++;
                 i--;
                 j--;
