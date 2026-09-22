d8313c55bda405dab11dd09673d35da5592b6947
Deeksha Agarwal <deeksha.agarwal_cs23@gla.ac.in>
2026-09-22 13:23:31 +0530

    Time: 14 ms (43.46%) | Memory: 53.3 MB (10.67%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,14 +1,13 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
+        for(int i = 0; i < nums.length; i++)
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
-        }
         ArrayList<Integer> l = new ArrayList<>();
-        for(int i : map.keySet()){
+        for(int i : map.keySet())
             if(map.get(i) > nums.length/3)
                 l.add(i);
-        }
+        
         return l;
     }
 }
